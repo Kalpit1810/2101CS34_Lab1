d743c4935b2247fbfc91f1ff6f9af20b4ce75626
kalpit1810 <kalpit1018@gmail.com>
2022-07-28 00:13:02 +0530

    added selection sort function and updated user interface in sort.c

--- sort.c
+++ sort.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 void ins();
+void sel();
 
 
 int main(){
     // creating user interface and taking user input
     int op;
 
-    printf("Enter the number corresponind to the sort operation you want to perform:\n 1. insertion sort\n 2. Selection sort\n 3. Bubble sort\n 4. Merge sort\n 5. Quick sort ");
+    printf("Enter the number corresponind to the sort operation you want to perform:\n 1. insertion sort\n 2. Selection sort\n 3. Bubble sort\n 4. Merge sort\n 5. Quick sort \n");
     scanf("%d", &op);
 
      switch(op){
         case 1: ins();
+        case 2: sel();
      };
 
     return 0;
@@ -23,7 +25,7 @@ void ins()
     printf("Enter number of elements\n");
     scanf("%d", &n);
     int arr[n];
-    
+
     printf("Enter %d integers\n", n);
     for (i = 0; i < n; i++) 
     {
@@ -45,4 +47,37 @@ void ins()
     {
         printf("%d\n", arr[i]);
     }
+}
+
+void sel()
+{
+int n, i, j, position, swap;
+printf("Enter number of elements:\n");
+scanf("%d", &n);
+int a[100];
+
+printf("Enter %d Numbers\n", n);
+
+for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+
+for(i = 0; i < n - 1; i++)
+{
+    position=i;
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[position] > a[j])
+                position=j;
+        }
+    if(position != i)
+    {
+        swap=a[i];
+        a[i]=a[position];
+        a[position]=swap;
+    }
+}
+
+printf("Sorted Array:n");
+for(i = 0; i < n; i++)
+printf("%d\n", a[i]);
 }
