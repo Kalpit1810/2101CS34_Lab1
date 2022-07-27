dd54102a4e62b9ade8695863d15fc7a1c948e920
kalpit1810 <kalpit1018@gmail.com>
2022-07-27 23:51:43 +0530

    created User interface for sort.c

--- sort.c
+++ sort.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    // creating user interface and taking user input
+    int op;
+
+    printf("Enter the number corresponind to the sort operation you want to perform:\n 1. insertion sort\n 2. Selection sort\n 3. Bubble sort\n 4. Merge sort\n 5. Quick sort ");
+    scanf("%d", &op);
 
     return 0;
 }
