56477c3db9ba6de2ece570e91e48f9df8e24b2f1
kalpit1810 <kalpit1018@gmail.com>
2022-07-27 23:22:18 +0530

    created user interface and taking inputs

--- Calculator.c
+++ Calculator.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    
+    // creating user interface and taking inputs.
+    float num1, num2;
+    int op;
+
+    printf("Enter the number corresponind to the operation you want to perform:\n 1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n  ");
+    scanf("%d", &op);
+    printf("Enter the Numbers:\n Num1: ");
+    scanf("%f", &num1);
+    printf("\n Num2: ");
+    scanf("%f", &num2);
+
     return 0;
 }
+
